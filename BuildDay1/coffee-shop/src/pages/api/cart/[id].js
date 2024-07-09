2a4a8bb1f4a6ad4f9e33343b5f15b9cbf769cbc9
Philip Karanja <kpk.my.mailings@gmail.com>
2024-07-09 00:55:40 -0500

    Updated index.js

--- BuildDay1/coffee-shop/src/pages/api/cart/[id].js
+++ BuildDay1/coffee-shop/src/pages/api/cart/[id].js
@@ -2,13 +2,18 @@ import { db } from "@/server/db";
 
 export default function handler(req, res) {
   if (req.method === "DELETE") {
-    console.log("req query here", req.query);
     const id = req.query.id;
     const numberId = Number(id);
     db.cart.delete(numberId);
     res.status(200).json({ message: "cart item removed" });
-    // TODO #3 Implement the PUT method here and handle incrementing and decrementing the quantity of an item in the cart
+  } else if (req.method === "PUT") {
+    const id = req.query.id;
+    const numberId = Number(id);
+    const updatedCartItem = db.cart.updateById(numberId, req.body.quantity);
+    res.status(200).json({ message: "cart item updated", updatedCartItem });
   } else {
-    res.status(404).json({ message: "We only support DELETE requests" });
+    res
+      .status(404)
+      .json({ message: "We only support DELETE and PUT requests" });
   }
 }
