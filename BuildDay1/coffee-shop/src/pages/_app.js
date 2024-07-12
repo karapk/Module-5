0cd30c75bc86b384e3b8b986bc3d1e5c427fe958
Philip Karanja <kpk.my.mailings@gmail.com>
2024-07-12 02:33:24 -0500

    Update _app.js

--- BuildDay1/coffee-shop/src/pages/_app.js
+++ BuildDay1/coffee-shop/src/pages/_app.js
@@ -1,3 +1,9 @@
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+/*
+The App component is the root component for all pages in your application. It is useful for passing down additional data to the page. 
+In this case, we are passing down the pageProps to the Component.
+The purposed of this component is to wrap the entire application with a layout or context provider that is shared across all pages.
+*/
