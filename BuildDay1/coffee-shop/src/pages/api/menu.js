f59e04d1d15fef92c2b2ed64c3830aeb36461466
Philip Karanja <kpk.my.mailings@gmail.com>
2024-07-12 02:31:30 -0500

    Update menu.js

--- BuildDay1/coffee-shop/src/pages/api/menu.js
+++ BuildDay1/coffee-shop/src/pages/api/menu.js
@@ -9,3 +9,20 @@ export default function handler(req, res) {
     res.status(404).json({ message: "We only support GET requests" });
   }
 }
+
+//code explained
+
+/*
+
+
+
+
+- In this code snippet, we are creating an API route that will be used to get the menu items. 
+- The menu items are stored in the db object that we imported from the server/db.js file. The db object has a menu property that has a get method that returns the menu items.
+
+- The handler function is an Express.js style request handler that takes two arguments, req and res. The req argument is an object that contains information about the HTTP request that was made, and the res argument is an object that contains methods for sending a response to the client.
+
+- We check if the HTTP method of the request is GET (read) using the req.method property. If the method is GET, we call the db.menu.get() method to get the menu items and store them in the menuItems variable.
+
+Remember that the db object is a fake database that we created to simulate a real database. In a real application, you would use a real database like MongoDB, PostgreSQL, or MySQL to store your data. We will be using PRISMA to interact with a real database in the next class. 
+*/
