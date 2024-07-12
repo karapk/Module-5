f9e269bd98570bd6519b49accdd8a1849c82599a
Philip Karanja <kpk.my.mailings@gmail.com>
2024-07-12 14:03:40 -0500

    Update _document.js

--- BuildDay1/coffee-shop/src/pages/_document.js
+++ BuildDay1/coffee-shop/src/pages/_document.js
@@ -11,3 +11,9 @@ export default function Document() {
     </Html>
   );
 }
+
+// The Document component is used to modify the entire HTML document.
+// The Html component is used to wrap the entire document.
+// The Head component is used to modify the head of the document.
+// The Main component is used to wrap the main content of the document.
+
