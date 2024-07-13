022621bad405e630895947007938b987461372e5
Philip Karanja <kpk.my.mailings@gmail.com>
2024-07-13 10:38:35 -0500

    updated schema and seed.js

--- Day-7/starter-template/prisma/seed.js
+++ Day-7/starter-template/prisma/seed.js
@@ -3,7 +3,23 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function main() {
-  console.log(`Update me to seed data`);
+  const todos = [
+    {id: 1,
+      title: "Water the plants", description: "The plants are thirsty"},
+    {id: 2,
+      title: "Make todo app", description: "Create a todo app using Prisma"},
+    {id: 3,
+      title: "Update schema", description: "Add a new field to the schema"},
+    {id: 4,
+      title: "Create some seed data", description: "Seed the database with some data"},
+    {id: 5,
+      title: "create a route", description: "Create a route to fetch all todos"},
+  ];
+  for (const todo of todos) {
+    await prisma.todo.create({
+      data: todo,
+    });
+  }
 }
 
 main()
